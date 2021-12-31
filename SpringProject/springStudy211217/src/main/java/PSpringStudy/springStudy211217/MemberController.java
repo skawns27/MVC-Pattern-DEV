b7f3b960b6b09eac73c9d1cd19e5813161c43f67
남준섭 <skawns27@gmail.com>
2021-12-31 20:17:40 +0900

    211231: Spring Container 등록 [Component, Config: Bean] 정리

--- SpringProject/springStudy211217/src/main/java/PSpringStudy/springStudy211217/MemberController.java
+++ SpringProject/springStudy211217/src/main/java/PSpringStudy/springStudy211217/MemberController.java
@@ -8,8 +8,11 @@ import org.springframework.stereotype.Controller;
 public class MemberController {
 
     private final MemberService memberService;
-    @Autowired
-    public MemberController(MemberService memberService) {
+    @Autowired //생성자 주입(추천)
+   public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+    //@Autowired private MemberService memberService; : 필드주입(비추천) => 중간에 수정이 불가능하다
+//    public void setMemberService(MemberService memberService) {
+//      this.memberService = memberService; } : Setter주입 => public 때문에 중간에 불필요한 변경 가능성 있음
 }
