3ac0b8cfb8eda80008408154ddcd812ace54dadf
남준섭 <skawns27@gmail.com>
2022-01-04 20:59:27 +0900

    220104: list 기능 누락 추가 및 git branch 오류 해결

---
+++ SpringProject/springStudy211217/src/main/java/PSpringStudy/springStudy211217/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package PSpringStudy.springStudy211217.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
